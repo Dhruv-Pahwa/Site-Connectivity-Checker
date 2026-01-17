679d0d9f1fec39619fd205aa427dcf5c098394e2
Dhruv Pahwa <96496469+Dhruv-Pahwa@users.noreply.github.com>
2026-01-17 19:22:41 +0530

    Update site connectivity checker.py

--- site connectivity checker.py
+++ site connectivity checker.py
@@ -1,13 +1,10 @@
 import urllib.request as urllib
-
 def main(dhruv):
     print("Checking connectivity ")
-    
     response = urllib.urlopen(dhruv)
     print("Connected to" , dhruv, "succesfully")
     print("The response code was: ", response.getcode())
-
 print("This is a site connectivity checker program")
 input_url = input("Input the url of the site you want to check: ")
-
 main(input_url)
+
